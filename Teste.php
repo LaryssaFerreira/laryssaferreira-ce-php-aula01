06c6adf51bdc127337363f4825f8fae62a8de4ee
LaryssaFerreira <laryyferreira95@gmail.com>
2017-08-22 20:50:36 -0300

    aula01

--- Teste.php
+++ Teste.php
@@ -26,6 +26,12 @@
 </head>
   
 <body>
+
+<?php
+  $A=10;
+  $B=20;
+?>
+
   <h1> PHP - aula02 </h1>
   
   <h3> Considerando de A seja igual a 10  e B seja igual a 20, eis os seguintes calculos: </h3>
@@ -33,10 +39,54 @@
  <table>
  
   <tr>
-    
+      <th>  Operacao </th>
+      <th> Resultado </th>
   </tr>
   
+  <tr>
+      <td>  A+B </td>
+      <td> 
+      <?php 
+        echo $A+$B;
+      ?> </td>
+  </tr>
   
+  <tr>
+      <td>  A-B </td>
+      <td> 
+      <?php
+        echo $A -$B;
+        ?>
+      </td>
+  </tr>
+  
+  <tr>
+      <td> A X B </td>
+      <td> 
+       <?php
+        echo $A*$B;
+        ?>
+      </td>
+  </tr>
+  
+  <tr>
+      <td>  A/B </td>
+      <td> 
+      <?php
+        echo $A/$B;
+        ?>
+      
+      </td>
+  </tr>
+  
+  <tr>
+      <td>  B <sup>a </sup> </td>
+      <td> 
+      <?php
+        echo pow($base, $exponente);
+      ?>
+      </td>
+  </tr>
   
   
   
