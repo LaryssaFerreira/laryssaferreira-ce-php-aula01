739093d62eb033b245d4bcf159910bc493217d2a
LaryssaFerreira <laryyferreira95@gmail.com>
2017-08-22 20:31:22 -0300

    aula01

---
+++ Teste.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+<html>
+<head>
+
+<title> Exercicio aula02</title>
+  <meta charset = "utf-8">
+  
+  <style>
+    table {
+        border : 1px solid black;
+        margin : auto;
+    }
+ 
+  t{
+    border : 1px solid black; 
+  }
+ 
+    body {
+    text-align : center;
+   
+ }
+ 
+  </style>
+  
+</head>
+  
+<body>
+  <h1> PHP - aula02 </h1>
+  
+  <h3> Considerando de A seja igual a 10  e B seja igual a 20, eis os seguintes calculos: </h3>
+  
+ <table>
+ 
+  <tr>
+    
+  </tr>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+ </table>
+  
